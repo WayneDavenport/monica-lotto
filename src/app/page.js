7f077ba62e6074d61d604f2788274c914de63d0b
Turtvonpickleshoe <111666555+Turtvonpickleshoe@users.noreply.github.com>
2025-05-09 23:03:05 -0500

    Texas lotto working font color

--- src/app/page.js
+++ src/app/page.js
@@ -160,7 +160,7 @@ export default function Home() {
             className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
           >
             {games.map((game) => (
-              <option key={game.id} value={game.id}>
+              <option key={game.id} value={game.id} className="text-gray-800">
                 {game.name}
               </option>
             ))}
